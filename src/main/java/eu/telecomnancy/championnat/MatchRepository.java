5337aad42351d01cec6ba152112d95722145a332
Najib Aghenda <najib.aghenda@gmail.com>
2018-11-18 15:19:37 +0100

    Légère modification pour afficher les pages + sorting

--- src/main/java/eu/telecomnancy/championnat/MatchRepository.java
+++ src/main/java/eu/telecomnancy/championnat/MatchRepository.java
@@ -2,9 +2,10 @@ package eu.telecomnancy.championnat;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface MatchRepository extends CrudRepository<Match, Long> {
+public interface MatchRepository extends JpaRepository<Match, Long> {
 
     List<Match> findByIdEquipeAAndIdEquipeB(Long idEquipeA, Long idEquipeB);
 }
