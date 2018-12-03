61d490bcd28e9ee05c451cf09c6e90910ebca139
Najib Aghenda <najib.aghenda@gmail.com>
2018-12-03 22:20:03 +0100

    link resources

--- src/main/java/eu/telecomnancy/championnat/controller/MatchController.java
+++ src/main/java/eu/telecomnancy/championnat/controller/MatchController.java
@@ -2,6 +2,7 @@ package eu.telecomnancy.championnat.controller;
 
 import java.util.List;
 
+import org.springframework.hateoas.Resource;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,8 +10,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 
 import eu.telecomnancy.championnat.Match;
+import eu.telecomnancy.championnat.controller.MatchController;
 import eu.telecomnancy.championnat.exception.MatchNotFoundException;
 import eu.telecomnancy.championnat.repository.MatchRepository;
 
@@ -29,10 +33,16 @@ public class MatchController {
 	}	
 	
 	@GetMapping("/matches/{id}")
-	Match one(@PathVariable Long id) {
+	Resource<Match> one(@PathVariable Long id) {
 
-		return repository.findById(id)
+		Match match = repository.findById(id)
 			.orElseThrow(() -> new MatchNotFoundException(id));
+
+		return new Resource<>(match,
+			linkTo(methodOn(MatchController.class).one(id)).withSelfRel(),
+			linkTo(methodOn(MatchController.class).all()).withRel("employees"));
 	}
+	
+	
 	
 }
