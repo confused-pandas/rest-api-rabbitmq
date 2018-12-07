187ae5d02ff21a9a75569c73c04219811c5a4e8c
Sabrina Haddad <sabrina.haddad@telecomnancy.eu>
2018-12-07 13:53:00 +0100

    Add modif competition a finir

--- src/main/java/eu/telecomnancy/championnat/controller/CompetitionController.java
+++ src/main/java/eu/telecomnancy/championnat/controller/CompetitionController.java
@@ -3,6 +3,7 @@ package eu.telecomnancy.championnat.controller;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -13,22 +14,40 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import eu.telecomnancy.championnat.Competition;
+import eu.telecomnancy.championnat.Equipe;
 import eu.telecomnancy.championnat.assembler.CompetitionResourceAssembler;
+import eu.telecomnancy.championnat.assembler.EquipeResourceAssembler;
+import eu.telecomnancy.championnat.assembler.MatchResourceAssembler;
 import eu.telecomnancy.championnat.exception.CompetitionNotFoundException;
+import eu.telecomnancy.championnat.exception.EquipeNotFoundException;
 import eu.telecomnancy.championnat.repository.CompetitionRepository;
+import eu.telecomnancy.championnat.repository.EquipeRepository;
+import eu.telecomnancy.championnat.repository.MatchRepository;
 
 @RestController
 public class CompetitionController {
 	
 	private final CompetitionRepository repository;
 	private final CompetitionResourceAssembler assembler;
+	private final EquipeRepository equipeRepository;
+	private final MatchRepository matchRepository;
+	private final MatchResourceAssembler matchAssembler;
+	private final EquipeResourceAssembler equipeAssembler;
 	
-	CompetitionController(CompetitionRepository repository, CompetitionResourceAssembler assembler) {
+	CompetitionController(CompetitionRepository repository, CompetitionResourceAssembler assembler, EquipeRepository equipeRepository, EquipeResourceAssembler equipeAssembler,
+			MatchRepository matchRepository, MatchResourceAssembler matchAssembler) {
 		this.repository = repository;
+		this.matchRepository = matchRepository;
+		this.matchAssembler = matchAssembler;
 		this.assembler = assembler;
+		this.equipeAssembler = equipeAssembler;
+		this.equipeRepository = equipeRepository;
+		
 	}
-	public
-	Resources<Resource<Competition>> all() {
+
+	
+	@GetMapping("/competitions")
+	public Resources<Resource<Competition>> all() {
 
 		List<Resource<Competition>> competition = repository.findAll().stream()
 			.map(assembler::toResource)
@@ -46,7 +65,24 @@ public class CompetitionController {
 		return assembler.toResource(competition);
 	}
 	
-	//@GetMapping("/Competitions/{id}/matches")
+	/*@GetMapping("/competitions/{id}/equipes")
+	public Resource<Resource<Equipe>> res(@PathVariable Long id) {
+		List<Equipe> equipes = new ArrayList<Equipe>();
+		List<Resource<Equipe>> equipesResources;
+		
+		Competition competition = repository.findById(id).orElseThrow(() -> new CompetitionNotFoundException(id));
+		long[] idEquipe = competition.getListeIdEquipe();
+
+		for (int i=0; i< idEquipe.length; i++){
+			Equipe equipe = repository.findById(idEquipe[i]) .orElseThrow(() -> new EquipeNotFoundException(idEquipe[i]));
+			equipes.add(equipe);
+		}
+		equipesResources = equipes.stream().map(assembler::toResource).collect(Collectors.toList());
+		return new Resources<>(equipesResources,
+				linkTo(methodOn(EquipeController.class).all()).withSelfRel());
+		
+		
+	}*/
 
 	
 }
